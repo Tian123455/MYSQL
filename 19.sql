177bece04d60340735201d4712b162629a277a5a
田雪晴 <98009195+Tian123455@users.noreply.github.com>
2022-02-12 14:23:09 +0800

    Add files via upload

---
+++ 19.sql
@@ -0,0 +1,47 @@
+-- ************JDBC
+
+create database mydb16_idbc;
+use mydb16_idbc;
+create table if not exists student(
+sid int primary key auto_increment,
+sname varchar(20),
+age int
+);
+
+insert into student values(NULL,'宋江',30),(NULL,'武松',28),(NULL,'林冲',26);
+
+select * from student;
+
+-- 防止SQL注入
+create table if not exists user(
+	uid int primary key auto_increment,
+	username varchar(20),
+	password varchar(20)
+);
+
+insert into user values(NULL,'zhangsan','123456'),(NULL,'lisi','888888');
+
+-- SQL注入
+drop table if exists user;
+create table user(
+	uid int primary key auto_increment,
+	username varchar(20),
+	password varchar(20)
+);
+insert into user values(NULL, 'zhangsan','123456'),(NULL,'lisi','888888');
+
+
+
+-- ************pymysql操作
+create database mydb17_pymysql;
+use mydb17_pymysql;
+create table if not exists student(
+sid int primary key auto_increment,
+sname varchar(20),
+age int
+);
+
+insert into student values(NULL,'宋江',30),(NULL,'武松',28),(NULL,'林冲',26);
+
+
+select * from student;
